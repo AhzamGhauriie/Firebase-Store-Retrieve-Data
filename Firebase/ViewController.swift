f24c42cf43f4bf059843b1d1b989790c7c4ab6d4
Ahzam Ghauri <ahzambaba2@gmail.com>
2020-04-02 18:13:55 +0500

    Initial Commit

---
+++ Firebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Firebase
+//
+//  Created by iOS App Developer -APP on 4/2/20.
+//  Copyright © 2020 Mohammad Zawwar-APP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
